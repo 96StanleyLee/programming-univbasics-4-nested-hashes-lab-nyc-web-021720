be8b145e81951e8b03d7522e9851ae400c02b9ad
Stanley Lee <sachih@live.com>
2019-11-06 22:37:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,6 +66,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
+
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -82,6 +83,11 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {
+
+	known_for: "Ruby"
+	languages: ["LISP", "C"]
+}
 
 end
 
